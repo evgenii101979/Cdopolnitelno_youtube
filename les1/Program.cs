c70798281a431201e2e87b13136d106c70e7ae21
evgenii <evgenii.lyabov@mail.ru>
2022-10-03 19:39:21 +0300

    add les1

---
+++ les1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// урок 6 ютуба. конвертация строки в др. типы. исправление введеннной точки на запятую.
+
+using System.Globalization;
+string str = "1.9";
+
+NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+{
+    NumberDecimalSeparator = ".",
+};
+
+
+double a = Convert.ToDouble(str, numberFormatInfo);
+
+System.Console.WriteLine(a);
+
+
