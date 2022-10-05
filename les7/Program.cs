4a8ece29c463961b2ff5bedbb1cbc7ec8449f53e
evgenii <evgenii.lyabov@mail.ru>
2022-10-05 19:04:57 +0300

    add les7

---
+++ les7/Program.cs
@@ -0,0 +1,16 @@
+﻿// урок 7- ютуба. 
+//1. преобразование строк parse и tryparse.
+
+
+string str = "100";
+int a;
+bool result = int.TryParse(str, out a);
+
+if (result)
+{
+    System.Console.WriteLine("Операция успешна " + a);
+}
+else
+{
+    System.Console.WriteLine("Не удалось конвертировать");
+}
