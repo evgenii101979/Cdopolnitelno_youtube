04f55859cfdfe22d49958db7f4588de2b4330ce2
evgenii <evgenii.lyabov@mail.ru>
2022-10-09 20:01:54 +0300

    add dz2

---
+++ dz2/Program.cs
@@ -0,0 +1,11 @@
+﻿// проверка числа на четность
+System.Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)
+{
+    System.Console.WriteLine("Число " +a +" четное");
+}
+else
+{
+    System.Console.WriteLine("Число " +a +" нечетное");
+}
