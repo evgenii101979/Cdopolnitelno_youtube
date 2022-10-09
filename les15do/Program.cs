c27693f881b3baf16fd566afc7f53686328c38f8
evgenii <evgenii.lyabov@mail.ru>
2022-10-09 21:01:37 +0300

    add les15do

---
+++ les15do/Program.cs
@@ -0,0 +1,43 @@
+﻿// урок 15 условный оператор switch
+
+
+/*int a = int.Parse(Console.ReadLine());
+switch (a)
+{
+    case 1:
+    /*System.Console.WriteLine("Вы ввели число 1 ");// свитч смотрит по номеру кейса и введенному числу совпадение
+    break;*/
+/* case 2:
+     System.Console.WriteLine("Вы ввели число 1 или 2 ");
+     break;
+ default:
+     System.Console.WriteLine("ХЗ что вы ввели ");
+     break;
+}*/
+
+// вариант со строками
+
+/*string a = Console.ReadLine();
+switch (a)
+{
+    case "+":
+        System.Console.WriteLine("Вы ввели + ");// свитч смотрит по номеру кейса и введенному числу совпадение
+        break;
+    case "-":
+        System.Console.WriteLine("Вы ввели - ");
+        break;
+    default:
+        System.Console.WriteLine("ХЗ что вы ввели ");
+        break;
+}
+*/
+
+// вариант 
+static void Main(string[] args)   //должна выдавать список кейсов клавиатуры
+{
+    ConsoleKey consoleKey = Console.ReadKey().Key;
+    switch (consoleKey)
+    {
+        default:
+    }
+}
